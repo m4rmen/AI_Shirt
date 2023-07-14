85f05a795eed94fda0d0672edbb766f1520deb80
m4rmen <90349436+m4rmen@users.noreply.github.com>
2023-07-13 22:57:35 -0600

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,17 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+
+
+
+
+
+export { 
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab
+     };
